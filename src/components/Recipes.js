aed4dc0b21eb10c51bc4d264232bc51c7bda76c4
lajawanti <lajawanti.dhake@gmail.com>
2020-01-03 18:19:00 -0500

    some updates

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Recipes = (props) => (
+    
     <div className = "container">            
         <div className = "row">
 
@@ -11,6 +12,8 @@ const Recipes = (props) => (
                             <img className = "recipe-img" src = {recipe.recipe.image} alt =  {recipe.recipe.label}/>
                             <h4  style= {{fontSize: "22px", padding : "20px 5px", fontWeight: "bold"}}> {recipe.recipe.label.length < 20 ? `${recipe.recipe.label}` : `${recipe.recipe.label.substring(0, 25)}...` }</h4>                         
                             <button className = "recipe-btn"><a href = {recipe.recipe.url} target = "_blank">View &nbsp; Recipe</a></button>
+                            <label style = {{paddingTop : "16px", marginBottom :"0px"}}>Total Calories : {Math.floor(recipe.recipe.calories)}</label>
+
                         </div>
                     </div>
                 )
