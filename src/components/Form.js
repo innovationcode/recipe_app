95c9350f4fffc39716ca72f7d11daf8b8a86a453
lajawanti <lajawanti.dhake@gmail.com>
2020-01-03 17:16:20 -0500

    style problem fixed

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Form = (props) => (
     <form onSubmit = {props.getRecipe} style = {{marginBottom : "2rem"}}>
         <input className = "form-input" type = "text" name = "recipe"/>
-        <button className = "form-button"> Search</button>
+        <button className = "recipe-btn">Search</button>
     </form>
 );
 
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -6,16 +6,11 @@ const Recipes = (props) => (
 
             {props.recipes.map((recipe, index) => {
                 return (
-                    <div className = ".col-xs-4" key = {index}>
-                        <div className = "recipe-box" style = {{
-                                                                    height: "auto",
-                                                                    width: "300px",
-                                                                    marginBottom: "2rem"
-                                                               }}>
-                            <h3> {recipe.recipe.label}</h3>
-                            <img src = {recipe.recipe.image}/>
-                            <p><a href = {recipe.recipe.url} target = "_blank">Link to recipe</a></p>
-                            {/* <p>Publisher : {}</p> */}
+                    <div className = "col-md-4" key = {index}>
+                        <div className = "recipes-box" >
+                            <img className = "recipe-img" src = {recipe.recipe.image} alt =  {recipe.recipe.label}/>
+                            <h4  style= {{fontSize: "22px", padding : "20px 5px", fontWeight: "bold"}}> {recipe.recipe.label.length < 20 ? `${recipe.recipe.label}` : `${recipe.recipe.label.substring(0, 25)}...` }</h4>                         
+                            <button className = "recipe-btn"><a href = {recipe.recipe.url} target = "_blank">View &nbsp; Recipe</a></button>
                         </div>
                     </div>
                 )
