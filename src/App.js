185b9da360b4d0b2393e0dc7099b7641698e040b
lajawanti <lajawanti.dhake@gmail.com>
2020-01-03 12:23:26 -0500

    data to state and display

--- src/App.js
+++ src/App.js
@@ -6,14 +6,23 @@ const API_ID = process.env.REACT_APP_API_ID;
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 class App extends Component{
+  //In REACT 16 onwords ni need to use constructor, super call and this.state
+  state = {
+    recipes : [],
+  }
  
   getRecipe = async (event) => {
       event.preventDefault();
       const recipeName = event.target.elements.recipe.value;
-      const API_CALL = await fetch(`https://api.edamam.com/search?q=paneer&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=5&calories=591-722&health=alcohol-free`);
+      const API_CALL = await fetch(`https://api.edamam.com/search?q=paneer&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=10&calories=591-722&health=alcohol-free`);
       const data = await API_CALL.json();
 
-      console.log("getRecipe......", data.hits[0].recipe.label);
+      //console.log("getRecipe......", data.hits);
+      this.setState({
+        recipes : data.hits,
+      })
+      console.log("getRecipe......", this.state.recipes);
+
   }
 
   render() {
@@ -24,6 +33,14 @@ class App extends Component{
         </header>
 
         <Form getRecipe = {this.getRecipe}/>
+
+        {this.state.recipes.map((recipe) => {
+            return (<div>
+                      <p key = {recipe.recipe.label}> {recipe.recipe.label}</p>
+                      <img src = {recipe.recipe.image}/>
+                    </div>
+                    )
+        })}
       </div>
     );
   }
