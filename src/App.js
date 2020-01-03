e3acaa65f47c3ac4e06e391965b70bd28fc9b203
lajawanti <lajawanti.dhake@gmail.com>
2020-01-03 11:58:52 -0500

    data feched from api

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, {Component} from 'react';
 import './App.css';
 import Form from './components/Form.js'
 
+const API_ID = process.env.REACT_APP_API_ID;
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class App extends Component{
-  getRecipe = (event) => {
+ 
+  getRecipe = async (event) => {
       event.preventDefault();
       const recipeName = event.target.elements.recipe.value;
-      console.log("getRecipe......", recipeName);
+      const API_CALL = await fetch(`https://api.edamam.com/search?q=paneer&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=5&calories=591-722&health=alcohol-free`);
+      const data = await API_CALL.json();
+
+      console.log("getRecipe......", data.hits[0].recipe.label);
   }
+
   render() {
     return (
       <div className="App">
