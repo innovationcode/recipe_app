754ef2a78efe51d62234c5703f972e83a7c0fa45
lajawanti <lajawanti.dhake@gmail.com>
2020-01-06 23:55:05 -0500

    Added recipe component for route link

---
+++ src/components/Recipe.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Link } from "react-router-dom";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+class Recipe extends React.Component {
+  state = {
+    
+  }
+  
+  }
+  render() {
+    return (
+        <div className="container">
+            <h1>Recipe</h1>
+        </div>
+    );
+  }
+};
+
+export default Recipe;
