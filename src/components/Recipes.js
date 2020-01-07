78401e8171a4ad33993acbca74a3ce7d1aa05334
lajawanti <lajawanti.dhake@gmail.com>
2020-01-07 13:51:27 -0500

    link for recipe

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Recipes = (props) => (
     
@@ -11,7 +12,14 @@ const Recipes = (props) => (
                         <div className = "recipes-box" >
                             <img className = "recipe-img" src = {recipe.recipe.image} alt =  {recipe.recipe.label}/>
                             <h4  style= {{fontSize: "22px", padding : "20px 5px", fontWeight: "bold"}}> {recipe.recipe.label.length < 20 ? `${recipe.recipe.label}` : `${recipe.recipe.label.substring(0, 25)}...` }</h4>                         
-                            <button className = "recipe-btn"><a href = {recipe.recipe.url} target = "_blank">View &nbsp; Recipe</a></button>
+                            <button className = "recipe-btn">
+                                {/* <a href = {recipe.recipe.url} target = "_blank"> */}
+                                <Link to={{ 
+                                        pathname: `/recipe/${recipe.recipe.label}`,
+                                        state: { recipe: recipe.recipe.label }
+                                }}> View &nbsp; Recipe </Link>
+                            </button> 
+                            
                             <label style = {{paddingTop : "16px", marginBottom :"0px"}}>Total Calories : {Math.floor(recipe.recipe.calories)}</label>
 
                         </div>
