b64f124ead478bccf10ebe8ab17f34e7c5aabae5
lajawanti <lajawanti.dhake@gmail.com>
2020-01-06 23:57:49 -0500

    rendering Router.js not App.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import Router from "./components/Router";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
+ReactDOM.render(<Router />, document.getElementById('root'));
